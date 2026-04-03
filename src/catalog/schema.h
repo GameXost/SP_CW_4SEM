54cbb95140e619901f070d3d06b95b7caed274ca
GameXost <dsergey010206@gmail.com>
2026-04-03 22:58:24 +0300

    added general info and purpose for directories added task for CW fixed gitignore

---
+++ src/catalog/schema.h
@@ -0,0 +1,8 @@
+//
+// Created by GameXost on 03.04.2026.
+//
+
+#ifndef SP_CW_4SEM_SCHEMA_H
+#define SP_CW_4SEM_SCHEMA_H
+
+#endif //SP_CW_4SEM_SCHEMA_H
---
+++ src/core/types.h
@@ -0,0 +1,18 @@
+//
+// Created by GameXost on 03.04.2026.
+//
+
+// Общие типы, структуры и т.п
+
+#ifndef SP_CW_4SEM_TYPES_H
+#define SP_CW_4SEM_TYPES_H
+
+// типы данных в БД
+enum class DataType {
+    INT,
+    STRING,
+};
+
+
+
+#endif //SP_CW_4SEM_TYPES_H
