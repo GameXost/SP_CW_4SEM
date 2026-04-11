dc021cb739e3279391ea46a28925ac13aa7c9375
GameXost <dsergey010206@gmail.com>
2026-04-11 17:59:26 +0300

    почистил ридмишки, теперь все описания в одном добавил ast_contract.md - разъяснения по тому, как строится ast дерево и какие есть узлы

--- src/core/types.h
+++ src/core/types.h
@@ -7,12 +7,33 @@
 #ifndef SP_CW_4SEM_TYPES_H
 #define SP_CW_4SEM_TYPES_H
 
-// типы данных в БД
-enum class DataType {
+#include  <string>
+#include <variant>
+#include <optional>
+
+// Тип данных колонки
+enum class ColumnType {
     INT,
-    STRING,
+    STRING
 };
 
+// ограничение значений в колонках
+enum class Constraint {
+    NONE,           // обычная колонка, может быть null
+    NOT_NULL,       // низя быть null
+    INDEXED         // уникальное поле ну и индекс деревянный, null запрещен
+};
 
+// значение ячейки: null/int/string
+// monostate = null
+// по сути union, но с возможностью ничего не хранить - monostate
+using Value = std::variant<std::monostate, int, std::string>;
 
+// ссылка на таблицу из запроса, по сути просто её название
+// users    => db = пустое, table = users
+// db.users => db = db, table = users
+struct TableReference {
+    std::optional<std::string> db; // optional - может и не быть этого значения, типа: "db"/{}
+    std::string table;
+};
 #endif //SP_CW_4SEM_TYPES_H
