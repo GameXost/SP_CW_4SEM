65d6b5eb564ffcd370c3990ec1b09de4c384303f
GameXost <dsergey010206@gmail.com>
2026-04-11 22:00:20 +0300

    добавил структуры для AST дерева на данный момент реализованы структуры: возможные типы данных -> SQL выражений -> ASTNodes

---
+++ src/parser/ast.h
@@ -0,0 +1,123 @@
+//
+// Created by GameXost on 11.04.2026.
+//
+
+#ifndef CW_AST_H
+#define CW_AST_H
+
+#include <memory>
+#include <string>
+#include <vector>
+#include <optional>
+#include "core/types.h"
+#include "parser/expr.h"
+
+// просто декларация
+struct Visitor;
+
+// базовый класс всех узлов нод
+struct ASTNode {
+    virtual void accept(Visitor&) = 0;
+    virtual ~ASTNode() = default;
+};
+
+using ASTNodePtr = std::unique_ptr<ASTNode>;
+
+
+// описание колонки в CREATE TABLE
+struct ColumnDef {
+    std::string name;
+    ColumnType type;
+    Constraint constraint;
+};
+
+// одна колонка в SELECT
+// SELECT name, age .... - два два SelectToColumn
+// SELECT *  - columns пустой вектор
+struct SelectColumn {
+    std::string name;
+    std::optional<std::string> alias;
+};
+
+
+// CREATE DATABASE name;
+struct CreateDatabaseStmt : ASTNode {
+    std::string name;
+    void accept(Visitor& v) override;
+};
+
+// DROP DATABASE name;
+struct DropDatabaseStmt : ASTNode {
+    std::string name;
+    void accept(Visitor& v) override;
+};
+
+// USE name;
+struct UseStmt : ASTNode {
+    std::string name;
+    void accept(Visitor& v) override;
+};
+
+// CREATE TABLE name (col1 TYPE CONSTRAINT, ...)
+struct CreateTableStmt : ASTNode {
+    TableReference table;
+    std::vector<ColumnDef> columns;
+    void accept(Visitor& v) override;
+};
+
+//DROP TABLE name;
+struct DropTableStmt : ASTNode{
+    TableReference table;
+    void accept(Visitor& v) override;
+};
+
+// INSERT INTO table (col1, col2) VALUE (v1, v2), (v3, v4);
+struct InsertStmt : ASTNode {
+    TableReference table;
+    std::vector<std::string> columns;
+    std::vector<std::vector<Value>> rows;
+    void accept(Visitor &) override;
+};
+
+// UPDATE table SET col1 = val1 WHERE condition;
+struct  UpdateStmt : ASTNode {
+    TableReference table;
+    std::vector<std::pair<std::string, Value>> assignments;
+    ExprPtr where;
+    void accept(Visitor &) override;
+
+};
+
+// DELETE FROM table WHERE condition;
+struct DeleteStmt : ASTNode {
+    TableReference table; // название таблицы
+    ExprPtr where;   //nullptr = нет WHERE
+    void accept(Visitor &) override;
+};
+
+// SELECT col1, col2 AS alias FROM table WHERE condition
+struct SelectStmt : ASTNode {
+    TableReference table;
+    std::vector<SelectColumn> columns; // пустой = SELECT *
+    ExprPtr where;
+    void accept(Visitor &) override;
+};
+
+// условный интерфейс для всех базовых SQL выражений, нужно реализовать в Executor
+struct Visitor {
+    virtual void visit(CreateDatabaseStmt&) = 0;
+    virtual void visit(DropDatabaseStmt&)   = 0;
+    virtual void visit(UseStmt&)            = 0;
+    virtual void visit(CreateTableStmt&)    = 0;
+    virtual void visit(DropTableStmt&)      = 0;
+    virtual void visit(InsertStmt&)         = 0;
+    virtual void visit(UpdateStmt&)         = 0;
+    virtual void visit(DeleteStmt&)         = 0;
+    virtual void visit(SelectStmt&)         = 0;
+    virtual ~Visitor() = default;
+};
+
+
+
+
+#endif //CW_AST_H
