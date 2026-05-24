824c0a016d4b1af20a410d61e497d4f08aaf11a0
Egor Drobyshev <playdro7100@gmail.com>
2026-05-24 16:58:31 +0300

    mini fix

--- src/index/index_manager.h
+++ src/index/index_manager.h
@@ -12,28 +12,10 @@
 
 class IndexManager {
 public:
-    void create(const std::string& db, const std::string& table, const std::string& column) {
-        (void)db; (void)table; (void)column;
-        // TODO: B*-tree + connect (db, table, column)
-    }
-
-    void drop(const std::string& db, const std::string& table, const std::string& column) {
-        (void)db; (void)table; (void)column;
-        // TODO: delete B*-tree and file
-    }
-
-    // unique checker (TODO after B*-tree)
-    void insert(const std::string& db, const std::string& table, const std::string& column,
-                const Value& key, RowId rid) {
-        (void)db; (void)table; (void)column; (void)key; (void)rid;
-        // TODO: tree.insert(key, rid); if key -> throw "Unique violation"
-    }
-
-    void erase(const std::string& db, const std::string& table, const std::string& column,
-               const Value& key) {
-        (void)db; (void)table; (void)column; (void)key;
-        // TODO: tree.erase(key)
-    }
+    void create(const std::string& db, const std::string& table, const std::string& column);
+    void drop(const std::string& db, const std::string& table, const std::string& column);
+    void insert(const std::string& db, const std::string& table, const std::string& column, const Value& key, RowId rid);
+    void erase(const std::string& db, const std::string& table, const std::string& column, const Value& key);
 
     std::optional<RowId> find(const std::string& db, const std::string& table, const std::string& column, const Value& key);
     std::vector<RowId> scanRange(const std::string& db, const std::string& table, const std::string& column, const Value& lo, const Value& hi);
